95946bc4b6c1cf7a6d2265de04f56957afaba5fe
kochol <cyrusgp@gmail.com>
2019-04-22 22:08:13 +0800

    update eth-abi

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'web3==4.7.2',
         'eth-utils==1.3.0',
-        'eth-abi==1.2.0',
+        'eth-abi==1.3.0',
         # TODO: This has to be removed when "ModuleNotFoundError: No module named 'eth_utils.toolz'" is fixed at eth-abi
         'python-dateutil==2.7.0',
         'click==6.7',
